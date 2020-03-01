aa97fd1f8a0a5ad970d98d946d32ddd9fb027ac6
Sergij Babich <moran.a@list.ru>
2020-03-01 13:11:30 +0200

    Updates

---
+++ precache-manifest.731a7dbcc1c5c10f700e7dc069956aed.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7df266d2556a6c628ad7118c297980c",
+    "url": "/easy-english/index.html"
+  },
+  {
+    "revision": "26cb7da2d81245b4edc0",
+    "url": "/easy-english/static/css/main.56a6cc5d.chunk.css"
+  },
+  {
+    "revision": "198deb71b70c4f557db9",
+    "url": "/easy-english/static/js/2.786ba07b.chunk.js"
+  },
+  {
+    "revision": "830c55e5e5effdba80df2f1b4bb9b6b9",
+    "url": "/easy-english/static/js/2.786ba07b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26cb7da2d81245b4edc0",
+    "url": "/easy-english/static/js/main.abb9c8c2.chunk.js"
+  },
+  {
+    "revision": "fe07ffa58a970e983345",
+    "url": "/easy-english/static/js/runtime-main.ee65ef05.js"
+  },
+  {
+    "revision": "4c2e9110ecbd29765a8933b1c7b036fe",
+    "url": "/easy-english/static/media/1.4c2e9110.jpg"
+  },
+  {
+    "revision": "91e68eb43ac9d76be3019f999e63a8ab",
+    "url": "/easy-english/static/media/10.91e68eb4.jpg"
+  },
+  {
+    "revision": "89fbfd984641358c3772698550b2bdb5",
+    "url": "/easy-english/static/media/11.89fbfd98.jpeg"
+  },
+  {
+    "revision": "1091c6302b42a005a721b1193262b474",
+    "url": "/easy-english/static/media/12.1091c630.jpg"
+  },
+  {
+    "revision": "6f606bf860c324e9e941d8b28f245289",
+    "url": "/easy-english/static/media/16.6f606bf8.jpg"
+  },
+  {
+    "revision": "f183abaaa347b4111d691e2d234adb48",
+    "url": "/easy-english/static/media/17.f183abaa.jpg"
+  },
+  {
+    "revision": "941c557f6c979ec42369a015b4803595",
+    "url": "/easy-english/static/media/18.941c557f.jpg"
+  },
+  {
+    "revision": "94adab4b85d6e46291e28b352c2ca6c0",
+    "url": "/easy-english/static/media/19.94adab4b.jpg"
+  },
+  {
+    "revision": "2f064de791369a8d1cd987e1a0f63b24",
+    "url": "/easy-english/static/media/2.2f064de7.jpg"
+  },
+  {
+    "revision": "f4b57891938a16427e766c10b1f63180",
+    "url": "/easy-english/static/media/21.f4b57891.jpg"
+  },
+  {
+    "revision": "30d9617a6f2dc1c2c0be62d9642e8f50",
+    "url": "/easy-english/static/media/22.30d9617a.jpeg"
+  },
+  {
+    "revision": "af470409740a04961bcd3eb69f6a2022",
+    "url": "/easy-english/static/media/23.af470409.jpg"
+  },
+  {
+    "revision": "b006fd1d38a442c7b821e03eaf3cf05f",
+    "url": "/easy-english/static/media/24.b006fd1d.jpg"
+  },
+  {
+    "revision": "e1a4695eb5ccc7c247a1329d17b7b73d",
+    "url": "/easy-english/static/media/25.e1a4695e.jpg"
+  },
+  {
+    "revision": "e1bfda06d3e5dfd11e46bb42ed729dc5",
+    "url": "/easy-english/static/media/26.e1bfda06.jpg"
+  },
+  {
+    "revision": "b1292fc5391f938990129704647160fb",
+    "url": "/easy-english/static/media/27.b1292fc5.jpg"
+  },
+  {
+    "revision": "dcb301b0b1fa01cc5dd18ee23912b32e",
+    "url": "/easy-english/static/media/28.dcb301b0.jpg"
+  },
+  {
+    "revision": "8c18b135504fd248152a9c276cd84f94",
+    "url": "/easy-english/static/media/29.8c18b135.jpg"
+  },
+  {
+    "revision": "4171aac2aa448a87d3dd0dfd1ea44f29",
+    "url": "/easy-english/static/media/3.4171aac2.jpg"
+  },
+  {
+    "revision": "bade136f710e4f72f0868adb59129b6e",
+    "url": "/easy-english/static/media/30.bade136f.jpg"
+  },
+  {
+    "revision": "1dd53b86332e14fb0e512b737343337a",
+    "url": "/easy-english/static/media/6.1dd53b86.jpg"
+  },
+  {
+    "revision": "aa3e646d4bb6a20e8aae95ff66d4a42f",
+    "url": "/easy-english/static/media/7.aa3e646d.jpg"
+  },
+  {
+    "revision": "8c4507b19fb92805eeda843ad30b4b22",
+    "url": "/easy-english/static/media/9.8c4507b1.jpeg"
+  },
+  {
+    "revision": "c8c8e4012872e3927e1fc209e9b4749b",
+    "url": "/easy-english/static/media/backgr_first_page.c8c8e401.jpg"
+  },
+  {
+    "revision": "1a05fb23ad80228df13f2884d76a18d6",
+    "url": "/easy-english/static/media/boy.1a05fb23.png"
+  },
+  {
+    "revision": "d8666d564244aea522d0e7da1f5b09d4",
+    "url": "/easy-english/static/media/desctiption.d8666d56.png"
+  },
+  {
+    "revision": "79f6e46a8e9389a79de1b71e0797d0de",
+    "url": "/easy-english/static/media/down_chevron.79f6e46a.png"
+  },
+  {
+    "revision": "d8a30a2b0dabe62f50e1828fdabb48c6",
+    "url": "/easy-english/static/media/kosmos.d8a30a2b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/easy-english/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e1a7e31d1ca4e838758edd57fd37d678",
+    "url": "/easy-english/static/media/user.e1a7e31d.png"
+  }
+]);
